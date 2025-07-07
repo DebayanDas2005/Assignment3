dfa8f9ce8bdd1b19e2ac6f61d3251608236bdf89
DebayanDas2005 <debayand324@gmail.com>
2025-07-07 20:05:32 +0530

    Add files via upload

---
+++ Using_the_Math_Module_for_Calculations.py
@@ -0,0 +1,14 @@
+num = int(input("Enter a number: "))
+import math
+def squareroot():
+   square_root = math.sqrt(num)
+   print("Square root:",square_root)
+def logarithim():
+    log_result = math.log(num)
+    print("Logarithim:",log_result)
+def sine():
+    sine_value = math.sin(num)
+    print("Sine:",sine_value)
+squareroot()
+logarithim()
+sine()
